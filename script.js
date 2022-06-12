74f1b4304ca9639df7db4e023f16157a51793159
Lex <webdevlex35@gmail.com>
2022-06-12 19:27:48 -0400

    update

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 
-let row1 = document.createElement('div');
-row1.classList.add('row1');
+let gridContainer = document.getElementsByClassName("gridContainer")[0];
 
-let container = document.querySelector(".grid");
-container.appendChild(row1);
 
-function createCell() {
-  let cell = document.createElement("div");
-  cell.classList.add("cell");
-  row1.appendChild(cell);
+function changeColor() {
+  this.style.backgroundColor = "black";
 }
 
-
-function createRow() {
-  for (let i = 0; i < 16; i++) {
-    createCell();
+function createGrid() {
+  for (let i = 0; i < 256; i++) {
+    let div = document.createElement('div');
+    div.classList.add('cell');
+    div.classList.add(i);
+    div.addEventListener("mouseover", changeColor);    
+    gridContainer.appendChild(div);
   }
 }
 
-createRow();
+
+
+createGrid();
+
+
+
+
+
+
+
+
+  
+
+
+
 
 
 
