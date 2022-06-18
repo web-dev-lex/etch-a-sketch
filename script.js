ad8a99b46d93def5be2e71f30f73fe74d2c6c8d0
Lex <webdevlex35@gmail.com>
2022-06-18 00:37:49 -0400

    update to javascript

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ function changeColor() {
 }
 
 function createGrid() {
-  for (let i = 0; i < 256; i++) {
+  clear();
+  let userInput = parseInt(prompt("Enter how many cells per side (1-100): "));
+  gridContainer.style.cssText += `grid-template-columns: repeat(${userInput}, 1fr); grid-template-rows: repeat(${userInput}, 1fr)`;
+  for (let i = 0; i < userInput * userInput; i++) {
     let div = document.createElement('div');
     div.classList.add('cell');
     div.classList.add(i);
@@ -16,9 +19,16 @@ function createGrid() {
   }
 }
 
-
+function clear() {
+  gridContainer.innerHTML = "";
+}
 
 createGrid();
+
+btn = document.getElementById("btn");
+btn.addEventListener("click", createGrid);
+
+
 
 
 
